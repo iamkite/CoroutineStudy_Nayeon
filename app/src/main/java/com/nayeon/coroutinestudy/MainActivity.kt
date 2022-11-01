27428440608c56f5a8b40e695bd21457b6175569
iamkite <iamkite0209@gmail.com>
2022-11-01 14:51:54 +0900

    [temp] 상세페이지 구현

--- app/src/main/java/com/nayeon/coroutinestudy/MainActivity.kt
+++ app/src/main/java/com/nayeon/coroutinestudy/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -16,7 +17,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.nayeon.coroutinestudy.api.Item
 import com.nayeon.coroutinestudy.ui.theme.CoroutineStudyTheme
 import com.skydoves.landscapist.glide.GlideImage
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,19 +42,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        SearchBar()
-                        SearchImage()
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = SEARCH_SCREEN_KEY) {
+                        composable(SEARCH_SCREEN_KEY) { SearchScreen(navController = navController) }
+                        composable(
+                            route = "${DETAIL_SCREEN_KEY}/{${IMAGE_ITEM_KEY}}",
+                            arguments = listOf(navArgument(IMAGE_ITEM_KEY) { type = NavType.ParcelableType(Item::class.java) })
+                        ) { backStackEntry ->
+                            DetailScreen(navController = navController, item = backStackEntry.arguments?.getParcelable(IMAGE_ITEM_KEY) ?: return@composable)
+                        }
                     }
                 }
             }
         }
     }
 
+    @Composable
+    fun SearchScreen(navController: NavController) {
+        Column {
+            SearchBar()
+            SearchImage(navController = navController)
+        }
+    }
+
     @Preview
     @Composable
     fun SearchBar() {
-        var searchText : String by remember { viewModel.searchText }
+        var searchText: String by remember { viewModel.searchText }
 
         Row(
             modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp)
@@ -64,26 +86,57 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @Preview
     @Composable
-    fun SearchImage() {
-        val linkList = viewModel.imageFlow.collectAsLazyPagingItems()
+    fun SearchImage(navController: NavController) {
+        val imageList = viewModel.imageFlow.collectAsLazyPagingItems()
 
         LazyVerticalGrid(
             columns = GridCells.Fixed(3),
             modifier = Modifier.fillMaxWidth()
         ) {
-            items(linkList.itemCount) { index ->
-                linkList[index]?.let { item ->
+            items(imageList.itemCount) { index ->
+                imageList[index]?.let { item ->
                     GlideImage(
                         imageModel = item.link,
                         modifier = Modifier
                             .wrapContentSize(Alignment.Center)
                             .fillMaxWidth()
                             .aspectRatio(1.0f)
+                            .clickable {
+                                navController.navigate("${DETAIL_SCREEN_KEY}/$item")
+                            }
                     )
                 }
             }
         }
+    }
+
+    @Composable
+    fun DetailScreen(navController: NavController, item: Item) {
+        DetailImage(navController = navController, thumbnailLink = item.thumbnail)
+        DetailText(title = item.title, width = item.sizeWidth, height = item.sizeHeight)
+    }
+
+    @Composable
+    fun DetailImage(navController: NavController, thumbnailLink: String) {
+        GlideImage(
+            imageModel = thumbnailLink,
+            modifier = Modifier
+                .clickable {
+                    navController.navigateUp()
+                }
+        )
+    }
+
+    @Composable
+    fun DetailText(title: String, width: Int, height: Int) {
+        Text(text = title)
+        Text("width : $width, height : $height")
+    }
+
+    companion object {
+        const val SEARCH_SCREEN_KEY = "searchScreen"
+        const val DETAIL_SCREEN_KEY = "detailScreen"
+        const val IMAGE_ITEM_KEY = "imageItem"
     }
 }
--- app/src/main/java/com/nayeon/coroutinestudy/api/Item.kt
+++ app/src/main/java/com/nayeon/coroutinestudy/api/Item.kt
@@ -1,11 +1,14 @@
 package com.nayeon.coroutinestudy.api
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Item(
     val title: String,
     val link: String,
     val thumbnail: String,
     @SerializedName("sizeheight") val sizeHeight: Int,
     @SerializedName("sizewidth") val sizeWidth: Int
-)
+) : Parcelable
